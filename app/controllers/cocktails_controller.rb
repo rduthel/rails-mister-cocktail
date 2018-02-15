2c268351c8b221c9b0ad054c3829a8d9cbdafa6b
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-15 16:11:59 +0100

    added the private set_... methods and ..._params

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,6 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: %i[index show]
+
   def index; end
 
   def show; end
@@ -6,4 +8,14 @@ class CocktailsController < ApplicationController
   def new; end
 
   def create; end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
