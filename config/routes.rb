db9f3283127940155122fc2d23af679497757c31
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-15 16:31:14 +0100

    generated a DosesController with new/create/destroy methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+
+  get 'doses/create'
+
+  get 'doses/destroy'
+
   resources :cocktails, only: %i[index show new create] do
     resources :doses, only: %i[new create destroy]
   end
