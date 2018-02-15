f084f04cb5507156e597fe001cad135e46813dd3
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-15 17:33:38 +0100

    created the create method

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -4,7 +4,17 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
-  def create; end
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
 
   def destroy
     Dose.find(params[:id]).destroy
@@ -13,6 +23,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
   end
 end
