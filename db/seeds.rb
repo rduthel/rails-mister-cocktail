7a17835949ebde7ec5ec9e74fce2b9248a7e8349
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-15 15:44:07 +0100

    removed the puts

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,6 @@ require 'open-uri'
 ingredients = JSON.parse(open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
 
 50.times do |i|
-  puts ingredients['drinks'][i]['strIngredient1']
   Ingredient.create!(
     name: ingredients['drinks'][i]['strIngredient1']
   )
