40de39bf02d17291e2b960e9c3b476c44ef06cea
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-15 16:59:33 +0100

    DosesController#new works

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,16 @@
 class DosesController < ApplicationController
   def new
+    @dose     = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
-  def create
-  end
+  def create; end
 
-  def destroy
+  def destroy; end
+
+  private
+
+  def dose_params
+    require(:dose).permit(:description)
   end
 end
