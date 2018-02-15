d023a803885adef76c51a6c45e13380cee4b1d54
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-15 16:21:31 +0100

    add the root path to the index method of CocktailsController

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :cocktails, only: %i[index show new create] do
     resources :doses, only: %i[new create destroy]
   end
+
+  root to: 'cocktails#index'
 end
